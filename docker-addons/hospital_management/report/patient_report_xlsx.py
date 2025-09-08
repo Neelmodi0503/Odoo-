7d8b643e869b11c79724cb501ffe520550531e8a
Neel Modi <neelmodi991@gmail.com>
2025-09-08 16:12:06 +0530

    changes

--- docker-addons/hospital_management/report/patient_report_xlsx.py
+++ docker-addons/hospital_management/report/patient_report_xlsx.py
@@ -29,57 +29,6 @@ class PatientReport(models.Model):
             row += 1
     
 
-class ReportInventory(models.Model):
-    _name = "report.hospital_management.inventory_stock_xlsx"
-    _description = "Report About Inventory"
-    _inherit = 'report.report_xlsx.abstract'
-
-
-    def generate_xlsx_report(self,workbook,data,quants):
-        sheet = workbook.add_worksheet('Inventory')
-        bold = workbook.add_format({'bold' : True, 
-                                    "align" : 'center',
-                                    'valign' : 'vcenter'})
-
-        sheet.write(0,0,"Product Name ",bold)
-        sheet.write(0,1,"0-30 days",bold)
-        sheet.write(0, 2, '31-60 days ', bold)
-        sheet.write(0,3,"60-90 days",bold)
-        sheet.write(0,4,"90+ days",bold)
-
-
-        today = date.today()
-        row = 1 
-        quants = self.env['stock.quant'].search([("quantity" , '>' , '0')])
-
-        for quant in quants:
-            day_0_30 = 0
-            day_31_60 = 0 
-            day_61_90 = 0 
-            day_90_up  = 0
-
-            age = (today - quant.in_date.date()).days
-
-            if age <= 30 :
-                day_0_30 = quant.quantity
-
-            elif age <= 60 :
-                day_31_60 = quant.quantity
-            
-            elif age <= 90 :
-                day_61_90 = quant.quantity
-
-            else :
-                day_90_up = quant.quantity
-
-            sheet.write(row, 0, quant.product_id.display_name)
-            sheet.write(row, 1, day_0_30)
-            sheet.write(row, 2, day_31_60)
-            sheet.write(row, 3, day_61_90)
-            sheet.write(row, 4, day_90_up)
-            row += 1
-
-
 
 
 
